c50334cc1ef713dfde5430cc39a6c6999bd857f4
Thani <62773552+ThaniSangarapillai@users.noreply.github.com>
2020-04-08 15:40:47 -0400

    changes parameters for read_file(), added custom pymavlink dialect

---
+++ GCS Client/gcsclient/templates/mav_util.py
@@ -0,0 +1,17 @@
+import sys, os
+
+sys.path.append(os.path.join(os.path.dirname(__file__)) + "\..\..\libraries")
+print(sys.path)
+
+from pymavlink_custom import mavutil
+
+def establish_connection():
+    uav_link = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
+
+    return uav_link
+
+def listen_link(uav_link):
+    try:
+        print(master.recv_match(type=["HEARTBEAT", "MAVIDS"]).to_dict())
+    except:
+        pass
